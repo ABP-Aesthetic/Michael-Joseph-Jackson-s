837d43dbeeee5868beab0bf4635e0cff890f80e5
ABP-Aesthetic <aliciablancope@alu.lapurisimavalencia.com>
2026-09-19 15:04:00 +0200

    Add searchPages array with page titles and URLs

---
+++ search.js
@@ -0,0 +1,46 @@
+const searchPages = [
+    {
+        title: "🕴 About Michael",
+        url: "about-michael.html"
+    },
+    {
+        title: "❤️ Life & Interests",
+        url: "life-and-interests.html"
+    },
+    {
+        title: "🎥 Videos",
+        url: "videos.html"
+    },
+    {
+        title: "📀 Discography",
+        url: "discography.html"
+    },
+    {
+        title: "💗 Moonwalkers' Space",
+        url: "moonwalkers-space.html"
+    },
+    {
+        title: "📔 Biography",
+        url: "biography.html"
+    },
+    {
+        title: "🗓 Timeline",
+        url: "timeline.html"
+    },
+    {
+        title: "🎤 Career",
+        url: "career.html"
+    },
+    {
+        title: "✨ Personality",
+        url: "personality.html"
+    },
+    {
+        title: "💝 Legacy",
+        url: "legacy.html"
+    },
+    {
+        title: "📑 More Facts",
+        url: "more-facts.html"
+    }
+];
