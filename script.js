0d4fa572fa86615879b49c194fa472eb76f745b6
ABP-Aesthetic <aliciablancope@alu.lapurisimavalencia.com>
2026-09-20 18:36:51 +0200

    Add event listener for create post button

--- script.js
+++ script.js
@@ -322,6 +322,12 @@ loggedInMessage.innerHTML = `
 
 loggedInArea.style.display = "block";
         document.getElementById("create-post-area").style.display = "block";
+        const createPostButton = document.getElementById("create-post-button");
+const createPostForm = document.getElementById("create-post-form");
+
+createPostButton.addEventListener("click", function () {
+    createPostForm.style.display = "block";
+});
         const logoutButton = document.getElementById("logout-button");
 
 logoutButton.addEventListener("click", async function () {
